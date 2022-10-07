fab0814e7fa3e2b04f98f495308d1d66b7393bff
Dennis Meyer <dennism3991@googlemail.com>
2022-10-07 11:41:52 +0200

    routing and page navigation

---
+++ Pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="about">About</NuxtLink>
+
+    <button @click="moveToAbout">Take me to About Page</button>
+  </div>
+</template>
+
+<script setup>
+function moveToAbout() {
+  navigateTo("about");
+}
+</script>
--- anything.vue
+++ anything.vue
@@ -2,19 +2,15 @@
   <div>
     <a href="#" @click="view('home')">Home</a>
     <a href="#" @click="view('counter')">Counter</a>
+
+    <NuxtPage />
+
     <LazyHomeWelcome v-if="page === 'home'" />
     <LazyCounter v-if="page === 'counter'" />
-    <!-- <component :is="MyComponent" /> -->
   </div>
 </template>
 
 <script setup>
-// const MyComponent= shallowRef(resolveComponent("Counter"))
-// const MyComponent = ref("counter");
-
-// function toggle() {
-//   MyComponent.value = "welcome";
-// }
 const page = ref("home");
 function view(name) {
   page.value = name;
