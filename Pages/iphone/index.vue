23eb37e4b96c30d8d27228ac2d6868532853a21e
Dennis Meyer <dennism3991@googlemail.com>
2022-10-07 13:30:24 +0200

    Head management

--- Pages/iphone/index.vue
+++ Pages/iphone/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>
-    <div>
+  <div class="grid grid-cols-4 gap-4 mt-10">
+    <nuxt-link to="/iphone/iphone-12" class="shadow-lg text-center">
       <h1>Iphone 12</h1>
-      <img width="200" src="/image/iphone12.png" alt="" />
+      <div class="flex border rounded-lg justify-center">
+        <img width="200" src="/image/iphone12.png" alt="" />
+      </div>
+    </nuxt-link>
+    <nuxt-link
+      to="/iphone/iphone-12-pro"
+      class="shadow-lg border rounded-lg text-center"
+    >
       <h1>Iphone 12 pro</h1>
-      <img width="200" src="/image/iphone12pro.jpg" alt="" />
-    </div>
+      <div class="flex justify-center">
+        <img width="200" src="/image/iphone12pro.jpg" alt="" />
+      </div>
+    </nuxt-link>
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: "Nuxt3 - iPhones",
+});
+</script>
--- Pages/iphone/iphone-[name].vue
+++ Pages/iphone/iphone-[name].vue
@@ -8,4 +8,7 @@ const route = useRoute();
 const name = computed(() => {
   return route.params.name.replaceAll("-", " ");
 });
+useHead({
+  title: `Nuxt3 - iPhone ${route.params.name}`,
+});
 </script>
