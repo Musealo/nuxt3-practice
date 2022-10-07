f988dd9075c26a116f2cf2734f3dede78eb7400d
Dennis Meyer <dennism3991@googlemail.com>
2022-10-07 13:00:37 +0200

    Tailwind assets

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav>
+    <nav class="bg-purple-700 text-white">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="about">About</NuxtLink>
       <NuxtLink to="profile">Profile</NuxtLink>
@@ -9,3 +9,7 @@
     <slot></slot>
   </div>
 </template>
+
+<script>
+import "assets/css/tailwind.css";
+</script>
