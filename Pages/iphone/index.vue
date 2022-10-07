4fb03c802a41c56bdc71b1066a54c397a57ff45c
Dennis Meyer <dennism3991@googlemail.com>
2022-10-07 13:15:33 +0200

    Public Directory

---
+++ Pages/iphone/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      <h1>Iphone 12</h1>
+      <img width="200" src="/image/iphone12.png" alt="" />
+      <h1>Iphone 12 pro</h1>
+      <img width="200" src="/image/iphone12pro.jpg" alt="" />
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
-    <nav class="bg-purple-700 text-white">
+    <nav
+      class="bg-purple-700 shadow-lg p-3 text-white text-lg flex justify-between"
+    >
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="about">About</NuxtLink>
+      <NuxtLink to="iphone">iPhones</NuxtLink>
       <NuxtLink to="profile">Profile</NuxtLink>
     </nav>
     <!-- here nuxt will replace page content -->
     <slot></slot>
   </div>
 </template>
-
-<script>
-import "assets/css/tailwind.css";
-</script>
