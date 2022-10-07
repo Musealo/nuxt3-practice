1685cdaa5aaad893bd91cf0c06debc91a782db04
Dennis Meyer <dennism3991@googlemail.com>
2022-10-07 11:26:45 +0200

    dynamic components and imports

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    Hello World
+    <a href="#" @click="view('home')">Home</a>
+    <a href="#" @click="view('counter')">Counter</a>
+    <LazyHomeWelcome v-if="page === 'home'" />
+    <LazyCounter v-if="page === 'counter'" />
+    <!-- <component :is="MyComponent" /> -->
   </div>
 </template>
+
+<script setup>
+// const MyComponent= shallowRef(resolveComponent("Counter"))
+// const MyComponent = ref("counter");
+
+// function toggle() {
+//   MyComponent.value = "welcome";
+// }
+const page = ref("home");
+function view(name) {
+  page.value = name;
+}
+</script>
---
+++ components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="increment">Increment</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">decrement</button>
+  </div>
+</template>
+
+<script setup>
+// option API Vue 2
+// export default {
+//   data() {
+//     return {
+//       count: 0,
+//     };
+//   },
+//   methods: {
+//     increment() {
+//       this.count++
+//     },
+
+//     decrement() {
+//       this.count--
+//     }
+//   },
+// };
+// VUE 3 composition API
+const count = ref(0);
+function increment() {
+  count.value++;
+}
+function decrement() {
+  count.value--;
+}
+</script>
